32805d95b1a42c9639d6284a1332d7103d1eec13
TymykO <152806676+TymykO@users.noreply.github.com>
2025-01-04 20:56:19 +0100

    Add combining data by time

--- combining_data.py
+++ combining_data.py
@@ -9,5 +9,19 @@ valve_data = valve_data_list("Valve/export-TA-Smart-TA-Smart 50 532290B7.csv")
 print(meteo_data[0])
 print(valve_data[0])
 
+data = []
+
+for x in meteo_data:
+    eu_time = datetime.strptime(x[1], '%Y-%m-%d %H:%M').replace(tzinfo=timezone.utc)
+    for y in valve_data:
+        #iso_time = datetime.fromisoformat(y[0])
+        if y[0] == 'Full Date':
+            data.append(y.append('Temperature outside [°C]'))
+        elif datetime.fromisoformat(y[0]) == eu_time:
+            data.append(y.append(x[2]))
+
+print(data[0])
+print(data[1])
+
 #combining_data_by_time
 
