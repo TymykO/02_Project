4c603e51ec1c92c32d7bebecf5774fd5e2589318
TymykO <152806676+TymykO@users.noreply.github.com>
2025-01-04 18:18:14 +0100

    Added excepts

--- loading_meteo_data.py
+++ loading_meteo_data.py
@@ -17,11 +17,19 @@ def read_csv_from_zip(zip_path: str, parameter_code: str, station_code: str):
                             if line[0] == station_code and line[1] == parameter_code:
                                 value = line[3].replace(',','.')
                                 data_station.append(f'{line[0]};{line[2]};{value}')
-                        return data_station
-    except:
-        print("Error")
+        return data_station
+    except IndexError:
+        print("Error Index")
+        return []
+    except FileNotFoundError:
+        print("Error File")
+        return []
+
+print(read_csv_from_zip('Meteo/Meteo_2023-01.zip', 'B00300S', '352200375')[0])
 
 if __name__ == "__main__":
 
-    assert read_csv_from_zip('Meteo/Meteo_2023-01.zip', 'B00300S', '352200375')[0] == '352200375;2023-01-01 00:00;13,9'
-    assert read_csv_from_zip('Meteo/Meteo_2023-01.zip', 'B00702A', '352200375')[0] == '352200375;2023-01-01 00:00;2,7'
+    assert read_csv_from_zip('Meteo/Meteo_2023-01.zip', 'B00300S', '352200375')[0] == '352200375;2023-01-01 00:00;13.9', print(" not OK1")
+    print("OK1")
+    assert read_csv_from_zip('Meteo/Meteo_2023-01.zip', 'B00702A', '352200375')[0] == '352200375;2023-01-01 00:00;2.7', print(" not OK2")
+    print("OK2")
