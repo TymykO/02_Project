68728634f14a9404b575c531c3a655658de7299e
TymykO <152806676+TymykO@users.noreply.github.com>
2025-01-04 16:50:47 +0100

    Cleaned code from garbage

--- loading_valve_data.py
+++ loading_valve_data.py
@@ -27,12 +27,8 @@ def valve_data_list(path_csv: str):
             else:
                 data = 'Incorrect valve data'
                 return data
-            a = 0
             for x in data_csv[2:]:
                 data.append(x)
-                a += 1
-                if a > 10:
-                    break
     except:
         # to do except
         pass
@@ -55,24 +51,19 @@ def valve_data_dict(path_csv: str):
             else:
                 data = 'Incorrect valve data'
                 return data
-            a = 0
             for x in data_csv[2:]:
                 row = []
                 for i, value in enumerate(keyword):
                     element = f'{value}: {x[i]}'
-                    print(element)
                     row.append(element)
                 data.append(row)
-                a += 1
-                if a > 10:
-                    break
     except:
         # to do except
         pass
     return data
 
-
-
-path = "Valve/export-TA-Smart-TA-Smart 50 532290B7.csv"
-
-print(valve_data_dict(path))
+if __name__ == "__main__":
+    assert valve_data_dict("Valve/export-TA-Smart-TA-Smart 50 532290B7.csv")[0] == ['Full Date: 2023-02-07T09:45:45+00:00', 'Year: 2023', 'Month: 2', 'Day: 7', 'Hour: 9', 'Minute: 45', 'Second: 45', 'Error Status: ', 'Control Mode: Position control', 'Override Status: None', 'Current Change-Over Regime: Regime 1', 'Temperature Limitation Status: Deactivated', 'Target flow for temp limitation (vs setpoint) [%]: 0.0', 'Actuator Position [mm]: 15.70', 'Measured Flow [l/h]: 3147.9', 'Measured Delta Temp. [K]: 16.9', 'Measured local Temp. [°C]: 58.5', 'Measured remote Temp. [°C]: 41.6', 'Measured Power [kW]: 61.19', 'Energy Counter Regime 1 [kWh]: 24', 'Energy Counter Regime 2 [kWh]: 0', 'Analog Input Signal [V]: 0.0', 'Bus Relative Setpoint [%]: 0.0', 'Relative Actuator position [%]: 100.0', 'Relative Measured Flow [%]: 23.5', 'Relative Measured Power [%]: 7.9'], "OK"
+    assert valve_data_list("Valve/export-TA-Smart-TA-Smart 50 532290B7.csv")[0] == ['Full Date', 'Year', 'Month', 'Day', 'Hour', 'Minute', 'Second', 'Error Status', 'Control Mode', 'Override Status', 'Current Change-Over Regime', 'Temperature Limitation Status', 'Target flow for temp limitation (vs setpoint) [%]', 'Actuator Position [mm]', 'Measured Flow [l/h]', 'Measured Delta Temp. [K]', 'Measured local Temp. [°C]', 'Measured remote Temp. [°C]', 'Measured Power [kW]', 'Energy Counter Regime 1 [kWh]', 'Energy Counter Regime 2 [kWh]', 'Analog Input Signal [V]', 'Bus Relative Setpoint [%]', 'Relative Actuator position [%]', 'Relative Measured Flow [%]', 'Relative Measured Power [%]'], "OK"
+    assert valve_data_dict("Valve/test.csv")[0] == 'Incorrect valve data', "OK"
+    assert valve_data_list("Valve/test.csv")[0] == 'Incorrect valve data', "OK"
